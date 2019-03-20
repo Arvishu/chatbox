9385cc878efa3bec81fc38029ff7975c6c882af3
hugo arvis <arvishugo@hotmail.frgit config --global user.email arvishugo@hotmail.fr>
2019-03-20 10:55:39 +0100

    first commit drop chatbox

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyDHuo7vvTsA1k0ZTh5qqB8jTxEIYH4yNbA",
+    authDomain: "chatbox-b3347.firebaseapp.com",
+    databaseURL: "https://chatbox-b3347.firebaseio.com"
+});
+
+export default base;
